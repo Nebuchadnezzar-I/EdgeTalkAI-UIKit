4b735a0ce6d690ef073c029a399ba34ab66a9ea0
Michal Ukropec <ukropec.michal.jr@gmail.com>
2025-05-13 13:24:48 +0200

    Initial Commit

---
+++ EdgeTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EdgeTalk
+//
+//  Created by Michal Ukropec on 13/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
